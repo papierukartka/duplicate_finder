d6a89f875ab04f8665bda819c609b436d490b7f3
Ś, Krzysztof <papierukartka@gmail.com>
2019-11-08 23:57:28 +0100

    flat is better than nested

--- tests/test_find.py
+++ tests/test_find.py
@@ -3,36 +3,39 @@ from duplicate_finder import find
 
 
 def test_dir_structure_should_return_valid_structure():
-    sample_directory_map = {
-        'sample_directory': {
-            'directories': {
-                'poznan': {
-                    'directories': {
-                        'secret': {
-                            'files': [
-                                'badtouch.jpg',
-                            ],
-                        },
-                    },
-                    'files': [
-                        'carrot.txt',
-                        'poznan.txt',
-                        'rot.txt',
-                        'test.txt',
-                    ],
-                },
-                'wroclaw': {
-                    'files': [
-                        'pic1.txt',
-                        'tomato.txt'
-                    ]
-                },
-            },
-            'files': [
-                'poznan.txt',
-            ]
-
+    directory = [
+        {
+            'file': './tests/sample_directory/poznan.txt',
+            'type': 'text'
+        },
+        {
+            'file': './tests/sample_directory/poznan/carrot.txt',
+            'type': '.txt',
+        },
+        {
+            'file': './tests/sample_directory/poznan/poznan.txt',
+            'type': '.txt'
+        },
+        {
+            'file': './tests/sample_directory/poznan/rot.txt',
+            'type': '.txt'
+        },
+        {
+            'file': './tests/sample_directory/poznan/test.txt',
+            'type': '.txt'
+        },
+        {
+            'file': './tests/sample_directory/poznan/secret/badtouch.jpg',
+            'type': '.jpg'
+        },
+        {
+            'file': './tests/sample_directory/wroclaw/pic1.txt',
+            'type': '.txt'
+        },
+        {
+            'file': './tests/sample_directory/wroclaw/tomato.txt',
+            'type': '.txt'
         }
-    }
+    ]
     find.dir_structure('./tests/sample_directory')
     pass
